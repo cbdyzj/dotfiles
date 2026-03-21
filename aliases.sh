45899d0e397370da7d5314983d3f4d6781e61c36
cbdyzj <cbdyzj@gmail.com>
2026-03-21 11:30:57 +0800

    feat: add ubuntu docker alias and remove extra blank lines

--- aliases.sh
+++ aliases.sh
@@ -5,15 +5,12 @@ alias tree='tree -N'
 alias iconv-gb18030='iconv -f gb18030'
 alias cwd='pwd | pbcopy'
 alias code='open -b com.microsoft.VSCode'
-
 alias ls-bun='bun -g list'
 alias ls-npm='npm -g ls --depth 0'
 alias ls-pnpm='pnpm -g ls --depth 0'
-
-
 alias pm2r="pm2 resurrect"
-
 alias use-python='export PATH="$HOME/Library/Python/3.9/bin:$PATH"'
-
 alias ai-ship="opencode run --command ship"
 alias claude-yolo='claude --dangerously-skip-permissions'
+alias ubuntu="docker run --rm -it public.ecr.aws/docker/library/ubuntu:24.04"
+
